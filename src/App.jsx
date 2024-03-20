a55c3963e381ae97ced92adb0f59e2ff911f6712
Alex Mendez <rmendezmartine00@mylangara.ca>
2024-03-20 00:50:05 -0700

    finished configuration of basics of app including routes, components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Education from './components/pages/Education';
+import Work from './components/pages/Work';
+import Projects from './components/pages/Projects';
 
-import ResponsiveAppBar from "./components/ResponsiveAppBar";
+function App() {
+    return (
+        <>
+            <Router>
+                <ResponsiveAppBar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    {/* <Route path='/education' element={<Education />} />
+                    <Route path='/work' element={<Work />} />
+                    <Route path='/projects' element={<Projects />} /> */}
+                </Routes>
+                {/* <Footer /> */}
+            </Router>
+        </>
+    );
+}
 
-const App = () => {
-  return (
-    <>
-      <ResponsiveAppBar />
-    </>
-  );
-};
+export default App;
