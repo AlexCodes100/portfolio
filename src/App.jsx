fc233bc5bf415431d7c3060010ab1942a368cb46
Alex Mendez <rmendezmartine00@mylangara.ca>
2024-03-23 01:03:09 -0700

    Routing Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
             <Router>
                 <ResponsiveAppBar />
                 <Routes>
-                    <Route path='/' element={<Home />} />
+                    <Route path='/home' element={<Home />} />
                     <Route path='/education' element={<Education />} />
                     <Route path='/work' element={<Work />} />
                     <Route path='/projects' element={<Projects />} />
